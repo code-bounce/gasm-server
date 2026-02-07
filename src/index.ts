b71844ebbf0109cd15f6821cf25fa3d5ddfe30c4
code-bounce <akhilrajktt@gmail.com>
2026-02-07 12:19:13 +0530

    build fix

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,8 @@ import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
 import { models } from "./models";
 
-const app = new Elysia({ prefix: "/api" })
+export default new Elysia({ prefix: "/api" })
   .use(cors())
   .get("/", () => "Hello Elysia")
   .use(models)
   .listen(3000);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-);
